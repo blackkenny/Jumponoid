2e6a9425ef3ebf51f1777833b554a25c1353f962
Bananicorn <ratschorr@gmail.com>
2018-04-22 19:34:15 +0800

    Changed makefile to actually include fonts on linux added property isDestructible to normal platforms (rects) added property isGoal to the goal block on top of the level

--- menu.lua
+++ menu.lua
@@ -1,7 +1,7 @@
 Menu = {}
 
 function Menu:draw()
-	font = love.graphics.newFont( "fonts/pac.ttf", 40 )
+	font = love.graphics.newFont("pac.ttf", 40 )
 	love.graphics.setFont(font)
 	love.graphics.print("Press Space to start a new game", 120, love.graphics.getHeight() / 2 - 50)
 end
