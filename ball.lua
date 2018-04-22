ff536504606089389857d8d9f320ad22e6f54c18
Thomas Toffalori <thomas.toffalori@gmail.com>
2018-04-22 13:03:30 +0200

    Add menu font

--- ball.lua
+++ ball.lua
@@ -2,13 +2,13 @@ Ball = {}
 Ball.__index = Ball
 
 function Ball:create(fizz, y)
-	local ball = fizz.addDynamic('circle', 20, y * BLOCKSIZE, 8)
+	local ball = fizz.addDynamic('circle', 20, y, 8)
 	ball.bounce = 1
 	ball.gravity = 0
 	ball.friction = 0
 	ball.damping = 0
 
-	fizz.setVelocity(ball, 1000, -1000)
+	fizz.setVelocity(ball, 500, -500)
 
 	setmetatable(ball, Ball)
 	return ball
--- menu.lua
+++ menu.lua
@@ -1,7 +1,9 @@
 Menu = {}
 
 function Menu:draw()
-	love.graphics.print("Press Space to start a new game", 10, 10)
+	font = love.graphics.newFont( "fonts/pac.ttf", 40 )
+	love.graphics.setFont(font)
+	love.graphics.print("Press Space to start a new game", 120, love.graphics.getHeight() / 2 - 50)
 end
 
 return Menu
