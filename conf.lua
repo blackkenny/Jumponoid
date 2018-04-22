c6a7ab03c1c7aa8072e170be0f97f7d8ab09bed3
Thomas Toffalori <thomas.toffalori@gmail.com>
2018-04-22 13:41:30 +0200

    Finalize menu

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
 	t.version = "11.0"
-	t.window.title = "template" --no need to change this, it's set to the name of the folder by make
+	t.window.title = "Jumponoid" --no need to change this, it's set to the name of the folder by make
 	t.window.width = 1280
 	t.window.height = 720
 end
--- menu.lua
+++ menu.lua
@@ -1,9 +1,18 @@
 Menu = {}
 
 function Menu:draw()
-	font = love.graphics.newFont( "fonts/pac.ttf", 40 )
-	love.graphics.setFont(font)
-	love.graphics.print("Press Space to start a new game", 120, love.graphics.getHeight() / 2 - 50)
+	titleFont = love.graphics.newFont( "fonts/pac.ttf", 120 )
+	love.graphics.setFont(titleFont)
+	love.graphics.print("JUMPONOID", love.graphics.getWidth() / 2 - 490, 100)
+	
+	subHeaderFont = love.graphics.newFont(18)
+	love.graphics.setFont(subHeaderFont)
+	love.graphics.print("Press space to start a new game", love.graphics.getWidth() / 2 - 150, love.graphics.getHeight() / 2)
+	
+	textFont = love.graphics.newFont(14)
+	love.graphics.setFont(textFont)
+	love.graphics.print("v0.0.0", love.graphics.getWidth() - 55, love.graphics.getHeight() - 25)
+	love.graphics.print("Bananicorn & Blackkenny ©2018", 15, love.graphics.getHeight() - 25)
 end
 
 return Menu
